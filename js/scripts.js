68b4fa5a12373d74435217565be642b4c2cb774e
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-09-29 17:15:16 -0400

    Added variables "favoriteColor" and "favoriteFood"

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ alert('Free Pizza!');
 let favoriteFood = "pizza";
 document.write(favoriteFood);
 
+let favoriteColor= "orange";
+document.write(favoriteColor);
 /*let size = 100;
 let doubleSize= size*2;
 let minSize=(doubleSize *2) - (size/2);
