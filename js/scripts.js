22e609d58daed9b47039af872788631864d9b23d
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-09-29 17:13:48 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+alert('Free Pizza!');
+
+let favoriteFood = "pizza";
+document.write(favoriteFood);
+
+/*let size = 100;
+let doubleSize= size*2;
+let minSize=(doubleSize *2) - (size/2);
+
+document.write(minSize); */
