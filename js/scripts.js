f1c0299bb674c5657534912130fe3fb3deceae51
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-09-30 21:05:40 -0400

    established pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
-alert('Free Pizza!');
+let pokemonList=[
 
-let favoriteFood = "pizza";
-document.write(favoriteFood);
 
-let favoriteColor= "orange";
-document.write(favoriteColor);
-/*let size = 100;
-let doubleSize= size*2;
-let minSize=(doubleSize *2) - (size/2);
 
-document.write(minSize); */
+
+]
