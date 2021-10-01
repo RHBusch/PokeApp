e95743709c5580fed0b0b21057ba138dc9b85e68
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-09-30 21:16:58 -0400

    Added pokemon to the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 let pokemonList=[
 
+{name:'Dewgong',
+  height: 1.7,
+  types:['ice','water']
+},
 
+{name:'Hitmonlee',
+  height:1.5,
+  types:['fighting']
+},
 
+{name:'Goldeen',
+  height:0.6,
+  types:['water']
+}
 
+{name:'Hypno',
+  height:1.6,
+  types:['psychic']
+}
 ]
