fe194cb75218c0b4148e789cdcb2f9246425c914
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-10-03 17:18:04 -0400

    Established conditional loop w/text

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,46 @@
+<script>
+  //write your code below here
+  //comment out other code blocks if you want to check output of a particular block
+
+
+
 // Establishing array for pokemonList
 let pokemonList=[
 //Building a list of multiple pokemon objects with ordered attributes and key-values).
 {name:'Dewgong',
-  height: 1.7,
-  types:['ice','water']
+ height: 1.7,
+ types:['ice','water']
 },
 
 {name:'Hitmonlee',
-  height:1.5,
-  types:['fighting']
+ height:1.5,
+ types:['fighting']
 },
 
 {name:'Goldeen',
-  height:0.6,
-  types:['water']
+ height:0.6,
+ types:['water']
 },
 
 {name:'Hypno',
-  height:1.6,
-  types:['psychic']
+ height:1.6,
+ types:['psychic']
 }
 ]
-for (let i=0; i < pokemonList.length; i++)
-{document.write(' '+ pokemonList[i].name+ ' ' + ' type ='+ pokemonList[i].types)};
+
+for (let i=0; i < pokemonList.length; i++){
+   let heightBigNote = 'Earth trembles... That\'s a BIG      pokemon!';
+   let psychicNote = 'Psychic pokemon are very rare!';
+
+   let pokemonName = pokemonList[i].name;
+   let pokemonHeight = pokemonList[i].height;
+   let pokemonTypes = pokemonList[i].types;
 
 
-for (let i=0; i < pokemonList.length; i++)
-if (pokemonList[i].types = 'psychic');
-{document.write(' ***That\'s a rare pokemon!***')}
+     {document.write(` <br><br> Pokemon Name: ${pokemonName}
+     <br> Height: ${pokemonHeight}
+     <br> Type(s): ${pokemonTypes}
+     `)
+     }
+     }
+</script>
