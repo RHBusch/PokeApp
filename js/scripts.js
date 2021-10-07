82b45d8e3bb329281899cd21480271b56c3ce1e4
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-10-07 18:33:49 -0400

    Added a foreach loop to my pokemonList2 array

--- js/scripts.js
+++ js/scripts.js
@@ -21,26 +21,25 @@ let pokemonList=[
 }
 ]
 
+/*
 //Creating a loop to scan the pokemon list above and feed data for output.
 /* The code below will scan the array for all items less than the
 length of the array (5 items) starting at 0.
 */
+
 for (let i=0; i < pokemonList.length; i++){
 
 //Assigning variables for my template literal
    let pokemonName = pokemonList[i].name;
    let pokemonHeight = pokemonList[i].height;
-/*Creating a conditional variable for my template literal heightText and psychic text willd
-depend on the if statements that follow */
+//Creating a conditional variable for my template literal heightText and psychic text willd depend on the if statements that follow */
    let heightText;
      if(pokemonHeight > 1.6){heightText= '- EARTH TREMBLES, that\'s a big pokemon!'};
    let pokemonTypes = pokemonList[i].types;
    let psychicText;
      if(pokemonTypes == 'psychic'){psychicText = '- psychic pokemon are very rare!'};
-/* The template literal below substitutes complicated strings for the variables assigned
-above. The `` are very important. 
 
-*/
+//template literal
      {document.write(`Pokemon Name: ${pokemonName}
      <br> Height: ${pokemonHeight}
      ${heightText || ''}
@@ -48,3 +47,34 @@ above. The `` are very important.
      `)
      }
      }
+
+//Using a foreach loop to iterate over my pokemonList2 array 10/07/21     
+
+let pokemonList2=[
+{name:'Dewgong',
+ height: 1.7,
+ types:['ice','water']
+},
+
+{name:'Hitmonlee',
+ height:1.5,
+ types:['fighting']
+},
+
+{name:'Goldeen',
+ height:0.6,
+ types:['water']
+},
+
+{name:'Hypno',
+ height:1.6,
+ types:['psychic']
+}
+];
+
+pokemonList2.forEach(function(getPokemonData)
+{console.log('Name: ' + getPokemonData.name + ', Height: ' + getPokemonData.height
++ ', Type(s): ' + getPokemonData.types)});
+
+//name.name is calling the function not the list
+//pokemonList is already being scrolled over
