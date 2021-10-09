10bbac0a90e732ca4717b65f4dc07257a8a46f63
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-10-09 11:37:55 -0400

    Established IIFE & updated .forEach loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-//Establishing an array for different types of pokemon
+let pokemonRepository = (function() {
+
 let pokemonList=[
 {name:'Dewgong',
  height: 1.7,
@@ -19,23 +20,33 @@ let pokemonList=[
  height:1.6,
  types:['psychic']
 }
-]
+];
 
-/*
-//Creating a loop to scan the pokemon list above and feed data for output.
-/* The code below will scan the array for all items less than the
-length of the array (5 items) starting at 0.
-*/
+function getAll() {return pokemonList};
+function add(pokemonNew){pokemonList.push(pokemonNew);
+}
+ return {
+    getAll: getAll,
+    add: add,
+  };
 
-for (let i=0; i < pokemonList.length; i++){
+})();
+
+/*Creating a loop to scan the pokemon list above and feed data for output.
+The code below will scan the array for all items less than the
+length of the array (5 items) starting at 0. */
+
+
+pokemonRepository.getAll().forEach(function(writePokemonData){
 
 //Assigning variables for my template literal
-   let pokemonName = pokemonList[i].name;
-   let pokemonHeight = pokemonList[i].height;
-//Creating a conditional variable for my template literal heightText and psychic text willd depend on the if statements that follow */
+   let pokemonName = writePokemonData.name;
+   let pokemonHeight = writePokemonData.height;
+/*Creating a conditional variable for my template literal heightText and psychic text willd depend on the if statements that follow */
+
    let heightText;
      if(pokemonHeight > 1.6){heightText= '- EARTH TREMBLES, that\'s a big pokemon!'};
-   let pokemonTypes = pokemonList[i].types;
+   let pokemonTypes = writePokemonData.types;
    let psychicText;
      if(pokemonTypes == 'psychic'){psychicText = '- psychic pokemon are very rare!'};
 
@@ -46,13 +57,10 @@ for (let i=0; i < pokemonList.length; i++){
      <br> Type(s): ${pokemonTypes} ${psychicText || ''}<br><br>
      `)
      }
-     }
+     })
 
 //Using a foreach loop to iterate over my pokemonList2 array 10/07/21
 
-pokemonList.forEach(function(getPokemonData)
-{console.log('Name: ' + getPokemonData.name + ', Height: ' + getPokemonData.height
+pokemonRepository.getAll().forEach(function(getPokemonData)
+{console.log('Name: ' + getPokemonData.name +', Height: ' + getPokemonData.height
 + ', Type(s): ' + getPokemonData.types)});
-
-//name.name is calling the function not the list
-//pokemonList is already being scrolled over
