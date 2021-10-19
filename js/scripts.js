95eeeef2cbe4866fe4e52b8cf6e9c2e0f3762e8a
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-10-19 17:32:09 -0400

    Updating js to call modal

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,8 @@ function addListItem (pokemon){
   let listItem = document.createElement('li');
 
 //Creating a button for each list item and assigning that button the innerText of the pokemon name.
-  let pokeButton = document.createElement('button');
+  let pokeButton =
+  document.createElement('button');
   pokeButton.innerText = pokemon.name;
 
 //Calling the eventListenerButton function to add an event listener for button clicks.
@@ -29,44 +30,11 @@ function addListItem (pokemon){
 
 //Adding the UL as the parent of the list.
   pokemonListScript.appendChild(listItem);
-  pokeButton.addEventListener("click", function(event){showDetails(pokemon)})
-}//closing addListItem function
+  }
 
-/*Creating a function with parameters for pokeButton and pokemon that creates
-an event listener for clicks and then calls the details function to
-log the pokemon name in the console that was clicked.*/
-
-  function eventListenerButton (pokeButton, pokemon){
-    pokeButton.addEventListener('click',function(){
-      showDetails(pokemon)})
+function eventListenerButton (pokeButton,pokemon){
+    pokeButton.addEventListener('click',function(){showDetails(pokemon)})
     };
-
-/*Logging the name of the pokemon in the console - setting up for calling
-showDetails as part of a button click.*/
-
-function showDetails (pokemon){
-  loadDetails(pokemon).then(function(){console.log(pokemon)})
-}
-//Creating a function that calls the pokemonList.
-function getAll() {return pokemonList};
-
-//Creating a function that adds a new pokemon to the list only if the 'item' is an object
-function add(pokemonNewItem){
-  typeof pokemonNewItem ==='object'?
-  pokemonList.push(pokemonNewItem) :
-//If the item trying to be added above is not an object, the message below will log in the console.
-  console.log ('Can\'t add a non-object')
-};
-
-/* A function designed to check if an item being added to the pokemon list has all the
-required keys. If it has all the keys the console will log the correct message */
-function checkKeys(pokemonNewItem){
-  if(Object.keys(pokemonNewItem).includes('name') &&
-  Object.keys(pokemonNewItem).includes('height') &&
-  Object.keys(pokemonNewItem).includes('types'))
-  {console.log('Okay to add - Pokemon has all included keys')}
-  else {console.log('Cannot add Pokemon- missing keys')}
-};
 
 function loadList (){
   return fetch(apiUrl).then(function(response){
@@ -94,8 +62,76 @@ function loadList (){
     }).catch(function(e){ // indicates what will happen if the promise is rejected
     console.error(e);
     });
-
   }
+
+function showDetails (pokemon){
+  loadDetails(pokemon).then(function(){showModal(pokemon.name, pokemon.height)
+  })};
+
+    let modalContainer = document.querySelector('#modal-container');
+    function showModal(name, height){
+      modalContainer.innerHtml = 'LOREM IPSUM';
+      let modal = document.createElement ('div');
+      modal.classList.add('modal');
+
+      let closeButtonElement = document.createElement('button');
+        closeButtonElement.innerText ='Close';
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.addEventListener ('click', hideModal)
+
+      let modalTitle = document.createElement ('h1');
+        modalTitle.innerText = 'NAME TBD';
+      let modalDetails = document.createElement ('p');
+        modalDetails.innerText = 'LOREM IPSUM LOREM IPSUM  LOREM IPSUM  LOREM IPSUM  LOREM IPSUM ';
+
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(modalTitle);
+      modal.appendChild(modalDetails);
+      modalContainer.appendChild(modal);
+      modalContainer.classList.add('is-visible');
+    }
+
+function hideModal(){
+    modalContainer.classList.remove('is-visible');
+
+
+  window.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+      hideModal();
+    }
+    }) //Closing addEventListener
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer){hideModal()};
+  })
+ document.querySelector('pokeButton').addEventListener('click',() =>{
+    showModal (pokemon.name, pokemon.height)
+  })
+
+}
+
+//Creating a function that calls the pokemonList.
+function getAll() {return pokemonList};
+
+//Creating a function that adds a new pokemon to the list only if the 'item' is an object
+function add(pokemonNewItem){
+  typeof pokemonNewItem ==='object'?
+  pokemonList.push(pokemonNewItem) :
+//If the item trying to be added above is not an object, the message below will log in the console.
+  console.log ('Can\'t add a non-object')
+};
+
+/* A function designed to check if an item being added to the pokemon list has all the
+required keys. If it has all the keys the console will log the correct message */
+
+function checkKeys(pokemonNewItem){
+  if(Object.keys(pokemonNewItem).includes('name') &&
+  Object.keys(pokemonNewItem).includes('height') &&
+  Object.keys(pokemonNewItem).includes('types'))
+  {console.log('Okay to add - Pokemon has all included keys')}
+  else {console.log('Cannot add Pokemon- missing keys')}
+};
 
   //Returning all relevant values for the functions above.
  return {
@@ -105,9 +141,9 @@ function loadList (){
     eventListenerButton: eventListenerButton,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails,
-
+     showDetails: showDetails,
   };
+
 })();
 
 //Calling the getAll and addListItem functions in a forEach loop to display the pokemon list.
